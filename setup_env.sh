028751ad7456d960ee6a068c0f8d6ea1dae477ae
Artem Garbart <garbart@yandex.ru>
2026-04-13 08:07:32 +0300

    Add setup script

--- setup_env.sh
+++ setup_env.sh
@@ -3,7 +3,7 @@ snap install ghostty --classic
 
 # install zsh
 apt install zsh
-chsh -s $(which zsh)
+chsh -s $(which zsh) $USER
 
 # install starship
 curl -sS https://starship.rs/install.sh | sh
@@ -12,9 +12,6 @@ echo "$(starship init zsh)" >> ~/.zshrc
 # eza
 apt install eza
 
-echo "alias ld='eza -lD'" >> ~/.zshrc
-echo "alias lf='eza -lF --color=always | grep -v /'" >> ~/.zshrc
-echo "alias lh='eza -dl .* --group-directories-first'" >> ~/.zshrc
-echo "alias ll='eza -al --group-directories-first'" >> ~/.zshrc
-echo "alias ls='eza -alF --color=always --sort=size | grep -v /'" >> ~/.zshrc
-echo "alias lt='eza -al --sort=modified'" >> ~/.zshrc
+echo "alias lh='eza -al --group-directories-first'" >> ~/.zshrc
+echo "alias ll='eza -l --group-directories-first'" >> ~/.zshrc
+echo "alias ls='eza -lF --color=always --sort=size | grep -v /'" >> ./zshrc
